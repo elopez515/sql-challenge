1eaf23fc7e9480c5c3bc2da5551ef4dc3d07b782
Efrain Lopez <e.lopez515@outlook.com>
2021-02-23 20:54:50 -0800

    Listed first name, last name, and hire date for employees who were hired in 1986

--- EmployeeSQL/Emp_Queries.sql
+++ EmployeeSQL/Emp_Queries.sql
@@ -7,3 +7,10 @@ SELECT employees.emp_no,
 	salaries.salary
 FROM employees, salaries
 WHERE employees.emp_no = salaries.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name,
+	employees.last_name,
+	employees.hire_date
+FROM employees
+WHERE employees.hire_date >= '1986-01-01' AND employees.hire_date <= '1986-12-31';
