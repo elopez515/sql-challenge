f562d23f6eb93dbcbfd550ec40fc37d14e5d0248
Efrain Lopez <e.lopez515@outlook.com>
2021-02-23 20:56:23 -0800

    Listed the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.

--- EmployeeSQL/Emp_Queries.sql
+++ EmployeeSQL/Emp_Queries.sql
@@ -14,3 +14,16 @@ SELECT employees.first_name,
 	employees.hire_date
 FROM employees
 WHERE employees.hire_date >= '1986-01-01' AND employees.hire_date <= '1986-12-31';
+
+-- List the manager of each department with the following information:
+-- -- department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no,
+	departments.dept_name,
+	dept_managers.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM departments
+	JOIN dept_managers ON
+	dept_managers.dept_no=departments.dept_no
+		JOIN employees ON
+		employees.emp_no = dept_managers.emp_no;
